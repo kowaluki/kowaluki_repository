fdfdbf1b4b97d96bcb535c03772f66de3d6641d8
Łukasz Kowalski <kowaluki1@gmail.com>
2022-05-08 22:32:19 +0200

    mysql inne dane

--- core/mysql.php
+++ core/mysql.php
@@ -1,6 +1,10 @@
 <?php
-    $mdb_host  = "mysql31.mydevil.net";
-    $mdb_user = "m1482_rekruter";
-    $mdb_pass = "Rmw.G2[f_CO]k95/3E06DInPVVkvL1";
-    $mdb_name = "m1482_rekrutacja";
+    // $mdb_host  = "mysql31.mydevil.net";
+    // $mdb_user = "m1482_rekruter";
+    // $mdb_pass = "Rmw.G2[f_CO]k95/3E06DInPVVkvL1";
+    // $mdb_name = "m1482_rekrutacja";
+    $mdb_host  = "127.0.0.1";
+    $mdb_user = "root";
+    $mdb_pass = "";
+    $mdb_name = "arts";
 ?>
